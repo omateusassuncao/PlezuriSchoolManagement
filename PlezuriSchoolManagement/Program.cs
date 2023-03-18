077c3ce853cc2fa0d3dbe6048de8e0ac67fecabe
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-03-18 09:49:13 -0300

    Correção do menu para looping

--- PlezuriSchoolManagement/Program.cs
+++ PlezuriSchoolManagement/Program.cs
@@ -5,37 +5,18 @@ using System.Runtime.ConstrainedExecution;
 List<Escola> listaEscolas = new List<Escola>();
 
 Console.WriteLine("Olá. Seja bem vindo@ ao Plézuri School Management App!");
-Console.WriteLine("Digite um valor relativo a opção do menu abaixo:");
 
-Console.WriteLine("Menu:");
-Console.WriteLine("1. Escolas - Cadastrar nova Escola");
-Console.WriteLine("2. Escolas - Editar uma Escola");
-Console.WriteLine("3. Escolas - Deletar uma Escola");
-Console.WriteLine("4. Alunos - Cadastrar nov@ Alun@");
-Console.WriteLine("5. Alunos - Editar um Alun@");
-Console.WriteLine("6. Alunos - Deletar um Escol@");
+while (ShowMenu());
 
-int opcao = int.Parse(Console.ReadLine());
-
-switch (opcao) {
-    case 1:
-        CriarEscola();
-    break;
-    //case 2:
-    //    escola.EditarCadastro();
-    //break;
-    //case 3:
-    //    escola.DeletarEscola();
-    //break;
-    //case 4:
-    //    Aluno aluno = new Aluno();
-    //break;
-    //case 5:
-    //    aluno.EditarAluno();
-    //break;
-    //case 6:
-    //    aluno.DeletarALuno();
-    //break;
+void EditarCadastroEscola()
+{
+    Console.WriteLine("Escolha uma escola:");
+    int i = 1;
+    foreach (Escola escola in listaEscolas)
+    {
+        Console.WriteLine(i + " " + escola.Nome);
+        i++;
+    }
 }
 
 void CriarEscola()
@@ -58,4 +39,51 @@ void CriarEscola()
     listaEscolas.Add(new Escola(nome, descricao, endereco, estado, cidade, cep, tipo));
 
     Console.WriteLine("Escola criada: " + listaEscolas.Last().Nome);
+}
+
+bool ShowMenu()
+{
+    Console.WriteLine("Digite um valor relativo a opção do menu abaixo:");
+
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. Escolas - Cadastrar nova Escola");
+    Console.WriteLine("2. Escolas - Editar uma Escola");
+    Console.WriteLine("3. Escolas - Deletar uma Escola");
+    Console.WriteLine("4. Alunos - Cadastrar nov@ Alun@");
+    Console.WriteLine("5. Alunos - Editar um Alun@");
+    Console.WriteLine("6. Alunos - Deletar um Escol@");
+    Console.WriteLine("0. SAIR");
+
+    int opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            CriarEscola();
+            return true;
+        break;
+        case 2:
+            EditarCadastroEscola();
+            return true;
+        break;
+        //case 3:
+        //    escola.DeletarEscola();
+        //    return true;
+        //break;
+        //case 4:
+        //    Aluno aluno = new Aluno();
+        //    return true;
+        //break;
+        //case 5:
+        //    aluno.EditarAluno();
+        //    return true;
+        //break;
+        //case 6:
+        //    aluno.DeletarALuno();
+        //    return true;
+        //break;
+        default:
+            return false;
+        break;
+    }
 }
